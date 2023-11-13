1a74dd5ab42595cbf7e6f8e4f79b2dd584ee2a3c
JavaMB7 <barrieresmathieu@gmail.com>
2023-11-13 08:38:42 +0100

    first commit

---
+++ src/tdd/Game.java
@@ -0,0 +1,15 @@
+package tdd;
+
+public class Game {
+
+	public void roll(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int score() {
+
+		return 0;
+	}
+
+}
---
+++ test/tdd/GameTest.java
@@ -0,0 +1,24 @@
+package tdd;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+	
+	Game game;
+	
+	@BeforeEach
+	void init() {
+		game = new Game();
+	}
+	
+	@Test
+	void test_aucune_quilles() {
+		for(int i = 0; i<20; i++) {
+			game.roll(0);
+		}
+		assertEquals(0, game.score());
+	}
+}
